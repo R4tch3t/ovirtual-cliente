d71cdb77286607a695ea78261713b247186e8431
R4tch3t <bebetovictor@gmail.com>
2022-06-01 21:08:25 -0500

    Actualizacion montar api

--- variables/url.ts
+++ variables/url.ts
@@ -1,11 +1,11 @@
 
 //API relative url
-const server = "http://localhost:3000/"
-//const server = "https://apitest.uagro.mx:3000/"
+//const server = "http://localhost:3000/"
+const server = "https://apitest.uagro.mx/"
 
 const url = `${server}graphql`;
 const urlApi = `${server}api`
-const urlBase = "http://localhost:3001/";
+const urlBase = "https://apitest.uagro.mx:3000/";
 const urlSocket = server;
 
 export {url,urlApi,urlBase,urlSocket}
